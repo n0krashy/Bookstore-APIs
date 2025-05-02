dfd5129e803a39d6915c9ef954577e921449763e
n0krashy <n0krashy@yahoo.com>
2025-05-03 00:50:22 +0300

    Add method to read invalid test data from JSON files

--- src/test/java/bookstore/data/JsonDataReader.java
+++ src/test/java/bookstore/data/JsonDataReader.java
@@ -1,10 +1,14 @@
 package bookstore.data;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
 
 public class JsonDataReader {
     public static <T> T readData(String resourcePath, Class<T> clazz) {
@@ -18,4 +22,17 @@ public class JsonDataReader {
             throw new RuntimeException("Failed to read JSON data from: " + resourcePath, e);
         }
     }
+
+    public static List<Map<String, Object>> readInvalidData(String filePath) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            return mapper.readValue(
+                    new File("src/test/resources/" + filePath),
+                    new TypeReference<>() {
+                    });
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to read invalid test data from " + filePath, e);
+        }
+    }
+
 }
